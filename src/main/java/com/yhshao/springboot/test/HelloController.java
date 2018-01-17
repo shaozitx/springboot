eadc1dbdffebe773966a02df4e099749b7cd7ef3
shaoqi <1511989705@qq.com>
2018-01-17 17:50:04 +0800

    测试

--- src/main/java/com/yhshao/springboot/test/HelloController.java
+++ src/main/java/com/yhshao/springboot/test/HelloController.java
@@ -1,7 +1,21 @@
 package com.yhshao.springboot.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by shaoqi on 18/1/17.
  */
+
+@Controller
 public class HelloController {
+
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayhellow(){
+        return "hello wordddddld";
+
+    }
 }
--- src/main/java/com/yhshao/springboot/test/UserReditRestController.java
+++ src/main/java/com/yhshao/springboot/test/UserReditRestController.java
@@ -1,7 +1,22 @@
 package com.yhshao.springboot.test;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+
 /**
  * Created by shaoqi on 18/1/17.
  */
+@RestController
 public class UserReditRestController {
+
+
+    @RequestMapping("/userredit/{id}")
+    public Integer getLevel(@PathVariable String id){
+        return Integer.parseInt(id);
+
+    }
+
+
 }
