93adc8b4602cc2539011470c30e4ac02e7da4aad
shaoqi <1511989705@qq.com>
2018-01-19 10:11:34 +0800

    自定义时间格式

--- src/main/java/com/yhshao/springboot/config/JacksonConf.java
+++ src/main/java/com/yhshao/springboot/config/JacksonConf.java
@@ -1,7 +1,24 @@
 package com.yhshao.springboot.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
 /**
  * Created by shaoqi on 18/1/18.
  */
+@Configuration
 public class JacksonConf {
+
+    @Bean
+    public ObjectMapper getObjectMapper(){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        return objectMapper;
+
+    }
+
 }
