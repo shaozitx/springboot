3ab10d4dce3fbaa074ae2200a6d348dbb604990a
shaoqi <1511989705@qq.com>
2018-01-19 10:10:14 +0800

    pom添加模板

---
+++ src/main/java/com/yhshao/springboot/config/JacksonConf.java
@@ -0,0 +1,7 @@
+package com.yhshao.springboot.config;
+
+/**
+ * Created by shaoqi on 18/1/18.
+ */
+public class JacksonConf {
+}
---
+++ src/main/java/com/yhshao/springboot/controller/ErrorController.java
@@ -0,0 +1,7 @@
+package com.yhshao.springboot.controller;
+
+/**
+ * Created by shaoqi on 18/1/18.
+ */
+public class ErrorController {
+}
